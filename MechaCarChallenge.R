4c300a788ce1c8a3d5632418a039f9cec6e9d633
apaneser <56094764+apaneser@users.noreply.github.com>
2021-10-09 19:04:46 -0700

    update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,14 @@ Sus_Coil <- read.csv('Suspension_Coil.csv')
 Sus_Tot_Sum <- Sus_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups='keep')
 # Summary for the individual lots
 Sus_Lot_Sum <- Sus_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups='keep')
+
+t.test(Sus_Coil$PSI, mu=mean(Sus_Coil$PSI))
+
+lot1 <- subset(Sus_Coil, Manufacturing_Lot == 'Lot1')
+t.test(lot1$PSI, mu=mean(Sus_Coil$PSI))
+
+lot2 <- subset(Sus_Coil, Manufacturing_Lot =='Lot2')
+t.test(lot2$PSI, mu=mean(Sus_Coil$PSI))
+
+lot3 <- subset(Sus_Coil, Manufacturing_Lot == 'Lot3')
+t.test(lot3$PSI, mu=mean(Sus_Coil$PSI))
